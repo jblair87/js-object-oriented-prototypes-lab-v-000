335a2e016d72e2b0648e448bd28a6f8f75f1253d
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-10-08 17:40:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,19 +31,19 @@ function BoardMember(name, homeState, training){
 }
 
 BoardMember.prototype.veto = function(){
-console.log('No, I must disagree') };
+return 'No, I must disagree' };
 
 BoardMember.prototype.approve = function(){
-console.log('You can do that!') };
+return 'You can do that!' };
 
 BoardMember.prototype.doCharity = function(){
-console.log('I like to help people.') };
+return 'I like to help people.' };
 
 BoardMember.prototype.releasePressStatement = function(){
-console.log("You will see great things from Scuber.") };
+return "You will see great things from Scuber." };
   
 BoardMember.prototype.sayHi = function() {
-console.log(`Hi, my name is ${this.name}. I am from  ${this.homeState}, and I was trained in  ${this.training}.`)};
+return `Hi, my name is ${this.name}. I am from  ${this.homeState}, and I was trained in  ${this.training}.`};
   
 let polishedBoardMember = new BoardMember('Mr. Polished', 'New York', 'law');
   
