eb4041ad4d37c836ce66a861adfc4907690aee22
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-10-08 17:25:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,6 +31,10 @@ function BoardMember(name, homeState, training){
 }
 
 User.prototype;
+
+BoardMember.prototype.releasePressStatement = function(){
+    console.log("You will see great things from Scuber.");
+  }
   
 BoardMember.prototype.sayHi = function() {
     console.log(`Hi, my name is ${this.name}. I am from  ${this.homeState}, and I was trained in  ${this.training}.`);}
